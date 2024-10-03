c06e487beb042a4357ca3831ddbe50cfe4064b94
manumuno <manueljmarin@outlook.com>
2024-10-03 21:16:52 +0200

    First exercise

---
+++ Ejercicio1/Cuenta.cs
@@ -0,0 +1,40 @@
+﻿namespace Ejercicio1
+{
+    internal class Cuenta
+    {
+        private string titular = "";
+
+        private double cantidad = 0;
+        public string Titular { get { return titular; } set { titular = value; } }
+        public double Cantidad { get { return cantidad; } set { cantidad = value; } }
+
+        public Cuenta(string titular)
+        {
+            this.titular = titular;
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public void Ingresar(double cantidad)
+        {
+            if (cantidad > 0) this.cantidad = cantidad;
+            
+        }
+
+        public void Retirar(double cantidad)
+        {
+            this.cantidad -= cantidad;
+            if (this.cantidad < 0) this.cantidad = 0;
+        }
+
+
+        public override string ToString()
+        {
+            return $"El titular: {titular} tiene: {cantidad} ";
+        }
+    }
+}
---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ejercicio1;
+
+Cuenta primera = new Cuenta("Manuel");
+Cuenta segunda = new Cuenta("Javier", 10);
+
+primera.Ingresar(50);
+segunda.Ingresar(50);
+segunda.Retirar(70);
+
+Console.WriteLine(primera);
+Console.WriteLine(segunda);
